68b64952e2e9d9f3ac946209aebf91886241db00
Aluno <Aluno@LAB04-PC10>
2024-06-04 21:25:16 -0300

    Commit project Text02

---
+++ src/Text02.java
@@ -0,0 +1,13 @@
+public class Text02 {
+
+    public static void main(String[] args) {   //ínicio do código
+        System.out.println("1");  //método 
+        {
+            System.out.println("2");
+        }
+            System.out.println("3");
+            System.out.println("4");
+    
+            
+    }
+}
